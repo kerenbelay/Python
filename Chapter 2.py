d48e5da2e5d4e02500828af0959788d7f5d2707e
kerenbelay <45443425+kerenbelay@users.noreply.github.com>
2023-09-04 13:43:15 -0400

    Add files via upload

---
+++ Chapter 2.py
@@ -0,0 +1,70 @@
+# Simple Message
+name = "Keren Belay\n"
+print(name)
+
+nameOfJam = "Blueberry\n"
+print(nameOfJam)
+
+# Simple Messages
+siblings_name = "David Belay"
+print(siblings_name)
+
+siblings_name = "Ella Belay\n"
+print(siblings_name)
+
+# Personal Messages
+name = "keren belay"
+print(f"Hello {name.title()}, would you like to learn some Python today?\n")
+
+# Name Cases 
+name = "gidey yehudit belay"
+print(name.upper())
+print(name.lower())
+print(name.title())
+print("\n")
+
+# Famous Quote
+name = "albert einstein"
+print(f'{name.title()} once said, "A person who never made a mistake never tried anything new."\n')
+
+# Famous Quote 2
+famous_person = "Albert Einstein"
+message = f'{famous_person} once said, "A person who never made a mistake never tried anything new"\n '
+
+print(message)
+
+# Stripping Names
+name = " solomon belay  " 
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+print("\n")
+
+# Number Eight
+print(4+4)
+print("___________________")
+print(2*4)
+print("___________________")
+print(16-8)
+print("___________________")
+print(16/2)
+print("___________________\n")
+
+# Favorite Number
+fav_num = 3
+print(f"my favorite number is {fav_num}")
+
+# Zen of Python
+import this
+
+age = 13 
+print(age)
+age = age - 1
+print(age)
+
+age += 1
+print(age)
+
+num1 = 21
+num2 = num1 % 3
+print(num2)
